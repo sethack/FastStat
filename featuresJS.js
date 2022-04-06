a5b508bce538d0e46b8369e1832b2b3a762e3e2f
Cody DeWitt <codygdewitt@gmail.com>
2022-04-06 14:41:01 -0700

    added features page and supporting code

---
+++ featuresJS.js
@@ -0,0 +1,19 @@
+
+JSC.Chart('myChart', {
+    type: 'line',
+    title_label_text: 'Line Series Types',
+    xAxis: { scale_type: 'time' },
+    series: [
+      {
+        name: 'Date',
+        points: [
+          ['1/1/2022', 29.9],
+          ['1/2/2022', 71.5],
+          ['1/3/2022', 106.4],
+          ['2/6/2022', 129.2],
+          ['3/7/2022', 144.0],
+          ['4/8/2022', 176.0]
+        ]
+      }
+    ]
+});
